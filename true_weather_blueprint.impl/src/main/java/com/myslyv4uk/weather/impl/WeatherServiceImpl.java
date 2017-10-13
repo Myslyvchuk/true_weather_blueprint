37b6002b5aabf7ceee3e5407961457bc0fbc68a8
Bogdan Myslyvchuk <bogdan.myslyvchuk@ampard.com>
2017-10-13 14:49:02 +0300

    deleted declarative services annotation

--- true_weather_blueprint.impl/src/main/java/com/myslyv4uk/weather/impl/WeatherServiceImpl.java
+++ true_weather_blueprint.impl/src/main/java/com/myslyv4uk/weather/impl/WeatherServiceImpl.java
@@ -2,9 +2,6 @@ package com.myslyv4uk.weather.impl;
 
 import java.util.concurrent.ThreadLocalRandom;
 
-import org.osgi.service.component.annotations.Activate;
-import org.osgi.service.component.annotations.Deactivate;
-
 import com.myslyv4uk.weather.api.WeatherService;
 
 
@@ -12,15 +9,13 @@ public class WeatherServiceImpl implements WeatherService {
 
   @Override
   public int getCurrentTemperature(int from, int to) {
-    return  ThreadLocalRandom.current().nextInt(from, to + 1);
+    return ThreadLocalRandom.current().nextInt(from, to + 1);
   }
-  
-  @Activate
+
   public synchronized void start() {
     System.out.println("Service WeatherServiceImpl activated");
   }
 
-  @Deactivate
   public synchronized void stop() {
     System.out.println("Service WeatherServiceImpl deactivated");
   }
